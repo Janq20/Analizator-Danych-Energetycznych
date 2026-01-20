ed39bfe2791732a643a074452cc5b2fecdc4c7e7
Janq20 <janoxgra@gmail.com>
2026-01-20 08:44:12 +0100

    Implementacja funkcji czyszczącej i konwertującej liczby

--- Narzedzia.h
+++ Narzedzia.h
@@ -4,19 +4,33 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <algorithm> 
 
 using namespace std;
+
 class Narzedzia {
 public:
     static vector<string> rozdzielTekst(string linia, char separator) {
         vector<string> wynik;
         stringstream ss(linia);
         string kawalek;
-
         while (getline(ss, kawalek, separator)) {
             wynik.push_back(kawalek);
         }
         return wynik;
+    }
+
+    static float konwertujNaFloat(string tekst) {
+        tekst.erase(remove(tekst.begin(), tekst.end(), '\"'), tekst.end());
+
+        if (tekst.empty()) return 0.0f;
+
+        try {
+            return stof(tekst);
+        }
+        catch (...) {
+            return 0.0f; 
+        }
     }
 };
 
