74a4c6d9df5ad67985959084b8df64b1f2b086a0
Janq20 <janoxgra@gmail.com>
2026-01-20 08:45:03 +0100

    Dodanie struktury DataCzas i parsera daty

--- Narzedzia.h
+++ Narzedzia.h
@@ -4,9 +4,15 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <algorithm> 
+#include <algorithm>
 
 using namespace std;
+struct DataCzas {
+    int rok;
+    int miesiac;
+    int dzien;
+    string godzina;
+};
 
 class Narzedzia {
 public:
@@ -22,15 +28,26 @@ public:
 
     static float konwertujNaFloat(string tekst) {
         tekst.erase(remove(tekst.begin(), tekst.end(), '\"'), tekst.end());
-
         if (tekst.empty()) return 0.0f;
+        try { return stof(tekst); }
+        catch (...) { return 0.0f; }
+    }
+    static DataCzas parsujDate(string surowaData) {
+        auto czesci = rozdzielTekst(surowaData, ' ');
+        if (czesci.size() < 2) return { 2000, 1, 1, "00:00" };
 
-        try {
-            return stof(tekst);
-        }
-        catch (...) {
-            return 0.0f; 
-        }
+        string dataStr = czesci[0]; 
+        string godzStr = czesci[1]; 
+        auto dataLiczby = rozdzielTekst(dataStr, '.');
+        if (dataLiczby.size() < 3) return { 2000, 1, 1, "00:00" };
+
+        DataCzas wynik;
+        wynik.dzien = stoi(dataLiczby[0]);
+        wynik.miesiac = stoi(dataLiczby[1]);
+        wynik.rok = stoi(dataLiczby[2]);
+        wynik.godzina = godzStr;
+
+        return wynik;
     }
 };
 
