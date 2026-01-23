44389ec25ade6fd7638589c6e93c8021fa838789
Janq20 <janoxgra@gmail.com>
2026-01-23 12:18:34 +0100

    Dodanie Pozostałych plików projektu

--- Analizator-Danych-Energetycznych.cpp
+++ Analizator-Danych-Energetycznych.cpp
@@ -1,15 +1,14 @@
 ﻿#include <iostream>
 #include <string>
 #include <limits> 
-#include "include/Pomiar.h"
-#include "include/Drzewo.h"
-#include "include/Narzedzia.h"
-#include "include/MenedzerPlikow.h"
-#include "include/Analizator.h"
+#include "Pomiar.h"
+#include "Drzewo.h"
+#include "Narzedzia.h"
+#include "MenedzerPlikow.h"
+#include "Analizator.h"
 
 using namespace std;
 
-// Funkcja wyświetlająca dostępne opcje
 void wyswietlMenu() {
     cout << "\n==========================================" << endl;
     cout << "   ANALIZATOR DANYCH ENERGETYCZNYCH v1.0" << endl;
@@ -24,7 +23,6 @@ void wyswietlMenu() {
 }
 
 int main() {
-    // 1. Inicjalizacja obiektów
     BazaDanych db;
     MenedzerPlikow menedzer;
     Analizator analizator(db);
@@ -32,47 +30,40 @@ int main() {
     int wybor = 0;
     string nazwaPliku;
 
-    // 2. Główna pętla programu
     while (true) {
         wyswietlMenu();
         if (!(cin >> wybor)) {
             cout << "To nie jest liczba! Sprobuj ponownie." << endl;
-            cin.clear(); // Czyści flagę błędu
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             continue;
         }
 
         switch (wybor) {
         case 1:
-            cout << "Podaj nazwe pliku CSV (np. zadanie6.csv): ";
+            cout << "Podaj nazwe pliku CSV (np. Chart Export.csv): ";
             cin >> nazwaPliku;
             menedzer.wczytajPlik(nazwaPliku, db);
             break;
-
         case 2:
             analizator.wypiszStatystyki();
             break;
-
         case 3:
             cout << "Podaj nazwe pliku do zapisu (np. baza.bin): ";
             cin >> nazwaPliku;
             menedzer.zapiszBinarnie(nazwaPliku, db);
             break;
-
         case 4:
             cout << "Podaj nazwe pliku do odczytu (np. baza.bin): ";
             cin >> nazwaPliku;
             menedzer.wczytajBinarnie(nazwaPliku, db);
             break;
-
         case 5:
             cout << "Zamykanie programu..." << endl;
             return 0;
-
         default:
-            cout << "Nieznana opcja. Wybierz liczbe od 1 do 5." << endl;
+            cout << "Nieznana opcja." << endl;
         }
     }
-
     return 0;
 }
