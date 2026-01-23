304ad7907ea9a38df4d8e38ccca1b882ceeb19d8
Janq20 <janoxgra@gmail.com>
2026-01-23 11:24:15 +0100

    Testy czesc 2: Weryfikacja struktury drzewa i sortowania czasowego

--- Testy.cpp
+++ Testy.cpp
@@ -25,3 +25,34 @@ TEST(NarzedziaTest, ParsowanieDaty_BlednyFormat) {
     DataCzas wynik = Narzedzia::parsujDate("BlednyString");
     EXPECT_EQ(wynik.rok, 2000);
 }
+// --- CZESC 2: DRZEWO I BAZA ---
+
+TEST(BazaDanychTest, WyznaczanieCwiartki_Rano) {
+    BazaDanych db;
+    EXPECT_EQ(db.wyznaczIndeksCwiartki("01.01.2021 04:00"), 0);
+}
+
+TEST(BazaDanychTest, WyznaczanieCwiartki_Wieczor) {
+    BazaDanych db;
+    EXPECT_EQ(db.wyznaczIndeksCwiartki("01.01.2021 20:00"), 3);
+}
+
+TEST(DrzewoTest, SortowaniePomiary) {
+    auto c = make_shared<Cwiartka>();
+    auto p1 = make_shared<Pomiar>("08:15", 0, 0, 0, 0, 0);
+    auto p2 = make_shared<Pomiar>("08:00", 0, 0, 0, 0, 0);
+
+    c->dodajPomiar(p1);
+    c->dodajPomiar(p2);
+
+    EXPECT_EQ(c->pomiary[0]->czas, "08:00");
+}
+
+TEST(DrzewoTest, StrukturaHierarchii) {
+    BazaDanych db;
+    auto p = make_shared<Pomiar>("01.01.2021 08:00", 10, 20, 30, 40, 50);
+    db.dodajDane(2021, 1, 1, p);
+
+    auto dzien = db.lata[2021]->wezMiesiac(1)->wezDzien(1);
+    EXPECT_FALSE(dzien->cwiartki.empty());
+}
