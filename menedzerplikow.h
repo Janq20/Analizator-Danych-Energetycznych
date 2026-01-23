f4335fb7a506b3f6a75d745c87223d6e4307ef94
Janq20 <janoxgra@gmail.com>
2026-01-23 10:58:30 +0100

    Implementacja parsowania CSV,obsługa wyjatkow i zapisu do bazy

--- menedzerplikow.h
+++ menedzerplikow.h
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <memory>
 #include "Drzewo.h"
 #include "Narzedzia.h"
+#include "Pomiar.h"
 
 using namespace std;
 
@@ -25,21 +28,50 @@ public:
 
         string linia;
         int licznikLinii = 0;
+        int poprawne = 0;
+        int bledne = 0;
 
-        cout << "Rozpoczynam czytanie linii..." << endl;
+        cout << "Wczytywanie danych..." << endl;
 
         while (getline(plik, linia)) {
             licznikLinii++;
-
-            // Wymóg: Pierwsza linia to nag³ówek 
             if (licznikLinii == 1) continue;
-
-            // Zabezpieczenie przed pustymi liniami na koñcu pliku
             if (linia.empty()) continue;
+
+            try {
+                vector<string> dane = Narzedzia::rozdzielTekst(linia, ',');
+
+                if (dane.size() < 6) {
+                    throw runtime_error("Niekompletna linia");
+                }
+
+                string czas = dane[0];
+                float autoKonsumpcja = Narzedzia::konwertujNaFloat(dane[1]);
+                float eksport = Narzedzia::konwertujNaFloat(dane[2]);
+                float importEn = Narzedzia::konwertujNaFloat(dane[3]);
+                float pobor = Narzedzia::konwertujNaFloat(dane[4]);
+                float produkcja = Narzedzia::konwertujNaFloat(dane[5]);
+
+                DataCzas dt = Narzedzia::parsujDate(czas);
+
+                auto pomiar = make_shared<Pomiar>(czas, autoKonsumpcja, eksport, importEn, pobor, produkcja);
+                db.dodajDane(dt.rok, dt.miesiac, dt.dzien, pomiar);
+
+                poprawne++;
+
+            }
+            catch (...) {
+                bledne++;
+                logger.logujBlad("Blad parsowania linii", linia);
+            }
         }
 
         plik.close();
-        cout << "Koniec czytania. Przetworzono linii: " << licznikLinii << endl;
+
+        cout << "=== Raport wczytywania ===" << endl;
+        cout << "Linii ogolem: " << licznikLinii << endl;
+        cout << "Poprawne:     " << poprawne << endl;
+        cout << "Bledne:       " << bledne << endl;
     }
 };
 
